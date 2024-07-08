595999ebbb5b0c390a8ae1b8d15a1db12423b297
ommanirai <ommanirai27@gmail.com>
2024-07-08 08:02:58 +0545

    git commands

--- git.py
+++ git.py
@@ -4,10 +4,15 @@ version controll tool
 
 to set user in git
 command:
-git config --global user.name=""
-git config --global user.email=""
+git config --global user.name ""
+git config --global user.email ""
 
-
+git commands:
+1. git init
+2. git status
+3. git add filename => track single file
+    git add . => track whole application
+4. git commit -m "message"
 
 
 '''

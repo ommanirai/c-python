1a11061e188f16905398699d5dd03008a5cfadaf
ommanirai <ommanirai27@gmail.com>
2024-07-09 07:47:50 +0545

    commands

--- git.py
+++ git.py
@@ -13,6 +13,18 @@ git commands:
 3. git add filename => track single file
     git add . => track whole application
 4. git commit -m "message"
+5. git log
+6. git checkout <commit_hash_code>
+7. git checkout master
 
+cloud repository
+github
+=> account
+=> link local to remote
+=> push
+git push origin master
+
+gitlab
+bitbucket
 
 '''
